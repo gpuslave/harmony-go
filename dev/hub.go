f374e37269357b72b2b449c106bb375baaa5cb4f
Aleksei Nemtsev <shimanoee@yandex.ru>
2025-01-31 12:40:44 +0300

    configure basic http

--- dev/hub.go
+++ dev/hub.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	// "bytes"
-	// "log"
-	// "net/http"
-	// "time"
+	"flag"
+	"log"
+	"net/http"
 
 	"fmt"
 
@@ -49,6 +48,61 @@ func newHub() *Hub {
 	}
 }
 
+func (hub *Hub) run() {
+	for {
+		select {
+		case client := <-hub.register:
+			hub.clients[client] = true
+		case client := <-hub.unregister:
+			if _, ok := hub.clients[client]; ok {
+				delete(hub.clients, client)
+				close(client.send)
+			}
+		case message := <-hub.broadcast:
+			for client := range hub.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(hub.clients, client)
+				}
+			}
+		}
+
+	}
+}
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	http.ServeFile(w, r, "home.html")
+}
+
 func main() {
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+
+	http.HandleFunc("/", serveHome)
+	// http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	// 	serveWs(hub, w, r)
+	// })
+
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("Listend and serve", err)
+	}
 	fmt.Println("hello")
 }
