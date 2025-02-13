2d8d8aa32eb9764b281c9245c793b095814d8a40
Aleksei Nemtsev <shimanoee@yandex.ru>
2025-02-13 15:14:27 +0300

    add response request handling

--- dev/hub.go
+++ dev/hub.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
+	"time"
 
 	"fmt"
 
@@ -12,7 +14,11 @@ import (
 )
 
 const (
-	// writeWait = 10 * time.Second
+	writeWait = 10 * time.Second
+
+	pongWait = 60 * time.Second
+
+	pingPeriod = (pongWait * 9) / 10
 
 	maxMessageSize = 512
 )
@@ -22,12 +28,84 @@ var (
 	space   = []byte{' '}
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 type Client struct {
 	hub *Hub
 
 	conn *websocket.Conn
 
 	send chan []byte
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+
+			w.Write(message)
+
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+
+		c.hub.broadcast <- message
+	}
 }
 
 type Hub struct {
@@ -88,28 +166,41 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	testFilePath := filepath.Join("web", "html", "test.html")
+	testFilePath := filepath.Join("web", "html", "home.html")
 	// homeFilePath := filepath.Join("web", "html", "home.html")
 
 	http.ServeFile(w, r, testFilePath)
 }
 
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
+}
+
 func main() {
 	fmt.Println("Init server...")
 	flag.Parse()
+
 	hub := newHub()
 	go hub.run()
 	fmt.Println("Hub successfully run.")
 
 	http.HandleFunc("/", serveHome)
-
-	// http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-	// 	serveWs(hub, w, r)
-	// })
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
 
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
-		log.Fatal("Listend and serve", err)
+		log.Fatal("Listen and serve", err)
 	}
 
 	fmt.Println("done.")
